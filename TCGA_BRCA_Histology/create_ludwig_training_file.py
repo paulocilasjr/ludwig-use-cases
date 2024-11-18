2f2ccf8d95f5c719b1b9cb8be96f21744944b1b7
PauloCilas.MoraisLyra <4475918@hpctpa3pl0003.cm.cluster>
2024-11-17 19:10:30 -0500

    slurm training files

--- TCGA_BRCA_Histology/create_ludwig_training_file.py
+++ TCGA_BRCA_Histology/create_ludwig_training_file.py
@@ -6,15 +6,21 @@ def create_ludwig_inputs(samples_df):
     '''
     Create a dataframe with the image paths, labels, and sample name for Ludwig.
     '''
+    count = 0
     ludwig_df = pd.DataFrame(columns=['image_path', 'er_status_by_ihc', 'sample'])
     for row in samples_df.itertuples():
         sample_dir = Path(row.filename).stem
-        tiles = os.listdir(os.path.join(sample_dir, sample_dir + '_tiles'))
+        try:
+            tiles = os.listdir(os.path.join(sample_dir, sample_dir + '_tiles'))
+        except FileNotFoundError:
+            print(f'Directory not found for sample: {row.filename}.')
+            count += 1
+            continue
         # Add each tile to the training data.
         for tile in tiles:
             tile_path = os.path.join(sample_dir, sample_dir + '_tiles', tile)
             ludwig_df.loc[len(ludwig_df)] = {'image_path': tile_path, 'er_status_by_ihc': row.er_status_by_ihc, 'sample': row.sample}
-
+    print(f'Total files not found: {count}')
     return ludwig_df
 
 # Read in samples manifest file.
--- TCGA_BRCA_Histology/tile_slides.sh
+++ TCGA_BRCA_Histology/tile_slides.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
 # Make images directory and copy all files there.
-mkdir images
-find . -type f -name '*.svs' -exec cp {} images/ \;
+# mkdir images
+# find . -type f -name '*.svs' -exec cp {} images/ \;
+
+input_list=$1
 
 # Loop through slides (*.svs files) and tile them.
-for file in images/*.svs; do
+#for file in images/*.svs; do
+while IFS= read -r file; do
     echo "Processing $file"
-    docker run -v $(pwd):/pyhist/images/ mmunozag/pyhist \
+    podman run -v $(pwd):/pyhist/images/ mmunozag/pyhist \
     --patch-size 512 \
     --content-threshold 0.4 \
     --output-downsample 4 \
@@ -14,5 +18,5 @@ for file in images/*.svs; do
     --percentage-bc 1 \
     --k-const 1000 \
     --minimum_segmentsize 1000 \
-    --save-patches --save-tilecrossed-image --info "verbose" --output images/ /pyhist/images/"$file"
-done
+    --save-patches --save-tilecrossed-image --info "verbose" --output images/  /pyhist/images/"$file"
+done < "$input_list"
