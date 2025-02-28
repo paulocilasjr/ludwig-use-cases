bd43a25d510cdc5be54253d78d8fdf14b2282fee
PauloCilas.MoraisLyra <4475918@hpctpa3pl0001.cm.cluster>
2025-02-27 20:51:23 -0500

    get all rows if no argument

--- TCGA_BRCA_Histology/sample_manifest.py
+++ TCGA_BRCA_Histology/sample_manifest.py
@@ -1,16 +1,22 @@
 import sys
 import pandas as pd
 
-# TODO: make argument parsing more robust.
-if len(sys.argv) != 2:
-    print('Usage: python sample_manifest.py <N>')
-    sys.exit(1)
-N = int(sys.argv[1])
-
-# Read manifest file.
+# Read manifest file
 df = pd.read_csv('final_TCGA_sample_manifest.txt', sep='\t')
 
-# Sample positive, negative from er_status_by_ihc column and write to file.
+# Filter for Positive and Negative values in er_status_by_ihc
 er_df = df[df['er_status_by_ihc'].isin(['Positive', 'Negative'])]
-er_df_sample = er_df.groupby('er_status_by_ihc').sample(n=N, random_state=1)
+
+# Check if an argument is provided
+if len(sys.argv) == 2:
+    try:
+        N = int(sys.argv[1])
+        er_df_sample = er_df.groupby('er_status_by_ihc').sample(n=N, random_state=1)
+    except ValueError:
+        print('Error: Argument must be an integer.')
+        sys.exit(1)
+except (ValueError, IndexError):
+    er_df_sample = er_df  # Use all available rows if no valid argument is given
+
+# Write output to file
 er_df_sample.to_csv('er_status_samples.txt', sep='\t', index=False)
